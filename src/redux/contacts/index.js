7485f15d0a5ee974098a9bf522a048d2df4e7037
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-18 13:03:16 +0300

    comments all redux logic for contacts

--- src/redux/contacts/index.js
+++ src/redux/contacts/index.js
@@ -1,4 +1,4 @@
-export * as contactsActions from "./contacts-actions";
-export * as contactsOperations from "./contacts-operations";
-export * as contactsReducer from "./contacts-reducer";
-export * as contactsSelectors from "./contacts-selectors";
+// export * as contactsActions from "./contacts-actions";
+// export * as contactsOperations from "./contacts-operations";
+// export * as contactsReducer from "./contacts-reducer";
+// export * as contactsSelectors from "./contacts-selectors";
